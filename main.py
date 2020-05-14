c275eff40ff70055d254e7fbd972d4c6bad5d855
thiskartik <gautam.kartik05@gmail.com>
2020-05-14 11:12:58 +0530

    table extraction successful

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from bs4 import BeautifulSoup
 
 # ======================
 
-def gotDirectResult(result,destUrl):
+def gotDirectResult(result,tempUrl):
     heading = result.get_text()
     print(heading)
-    print('Your page is at : '+destUrl)
+    print('Your page is at : '+tempUrl)
+    return tempUrl
 
 def gotSearchRes(firstResult):
     print(firstResult)
@@ -15,7 +16,13 @@ def gotSearchRes(firstResult):
     coUrl = firstResulta.get('href')
     final = wikiUrl+coUrl
     print(final)
+    return final
     
+def reqThenParse(page):
+    prs = requests.get(page)
+    html = prs.content
+    parsed = BeautifulSoup(html,'html.parser')
+    return parsed    
 # ======================
 
 urlSearch = 'https://en.wikipedia.org/w/index.php?cirrusUserTesting=control&search='
@@ -41,10 +48,41 @@ res = search.find('div', {'class': 'mw-search-result-heading'})
 # print('Search div result = ',res)
 
 if(res is not None):
-    gotSearchRes(res)
+    destUrl = gotSearchRes(res)
 elif(res is None):
     searchRes = search.find('h1')
     print(searchRes)
     #print(search)
     if(searchRes is not None):
-        gotDirectResult(searchRes,searchLink)
+        destUrl = gotDirectResult(searchRes,searchLink)
+
+#searching done        
+
+destPage = reqThenParse(destUrl)
+
+bioTable = destPage.find('table',{'class':'infobox biography vcard'})
+
+tr = bioTable.find_all('tr')
+
+left = []
+right = []
+for i in tr:
+    if(i.find('th') is not None):
+        if(i.find('td') is not None):
+            left.append(i.find_all('th'))
+            right.append(i.find_all('td'))
+
+keys = []
+for eleL in left:
+    for ele in eleL:
+        keys.append(ele.get_text())
+
+values = []
+for eleR in right:
+    for ele in eleR:
+        values.append(ele.get_text())
+
+generalTable = dict(zip(keys,values))
+
+print('TABLE = ')
+print(generalTable)
